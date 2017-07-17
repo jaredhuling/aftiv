93e1c22ceac4a0345ccf6bd2cdd124f47ae49934
jaredhuling <huling@wisc.edu>
2017-07-17 14:19:37 -0500

    cleaned up c++ est eqn calls

--- R/call_estimating_equations.R
+++ R/call_estimating_equations.R
@@ -1,35 +1,39 @@
 
 
-AFTScorePre.cpp <- function(beta, survival, X, ZXmat) {
+AFTScorePre.cpp <- function(beta, survival, X, ZXmat) 
+{
   
-  if (is.null(survival$log.t)) {
+  if (is.null(survival$log.t)) 
+  {
     log.t <- log(survival$t)
   } else {
     log.t <- survival$log.t
   }
   
-  err <- log.t - X %*% beta
+  err       <- log.t - X %*% beta
   order.idx <- order(err)
-  X <- as.matrix(X[order.idx,]) 
-  delta <- survival$delta
-  delta <- delta[order.idx]
+  X         <- as.matrix(X[order.idx,]) 
+  delta     <- survival$delta
+  delta     <- delta[order.idx]
   
   .Call("AFTScorePre", XX = X, delta_vec = delta, PACKAGE = "aftiv")
 }
 
-AFTivScorePre.cpp <- function(beta, survival, X, ZXmat) {
+AFTivScorePre.cpp <- function(beta, survival, X, ZXmat) 
+{
   
-  if (is.null(survival$log.t)) {
+  if (is.null(survival$log.t)) 
+  {
     log.t <- log(survival$t)
   } else {
     log.t <- survival$log.t
   }
   
-  err <- log.t - X %*% beta
+  err       <- log.t - X %*% beta
   order.idx <- order(err)
-  ZXmat <- as.matrix(ZXmat[order.idx,]) 
-  delta <- survival$delta
-  delta <- delta[order.idx]
+  ZXmat     <- as.matrix(ZXmat[order.idx,]) 
+  delta     <- survival$delta
+  delta     <- delta[order.idx]
   
   .Call("AFTScorePre", XX = ZXmat, delta_vec = delta, PACKAGE = "aftiv")
 }
