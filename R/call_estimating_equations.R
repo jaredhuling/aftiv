53fc0daae32452d60e90fbe1eba998e35c7fedb0
jaredhuling <huling@wisc.edu>
2017-07-20 14:51:50 -0500

    added ipcw R to c++ call

--- R/call_estimating_equations.R
+++ R/call_estimating_equations.R
@@ -47,3 +47,33 @@ AFTivScorePre.cpp <- function(beta, survival, X, ZXmat)
   .Call("AFTScorePre", XX = ZXmat, delta_vec = delta, PACKAGE = "aftiv")
 }
 
+
+AFTivIPCWScorePre.cpp <- function(beta, survival, X, ZXmat, GC) 
+{
+  
+  if (is.null(survival$log.t)) 
+  {
+    log.t <- log(survival$t)
+  } else 
+  {
+    log.t <- survival$log.t
+  }
+  
+  stopifnot(length(beta) == NCOL(X))
+  stopifnot(NROW(X) == NROW(ZXmat))
+  stopifnot(NROW(X) == length(survival$delta))
+  
+  err       <- log.t - X %*% beta
+  order.idx <- order(err)
+  ZXmat     <- as.matrix(ZXmat[order.idx,, drop = FALSE]) 
+  delta     <- survival$delta
+  delta     <- delta[order.idx]
+  
+  #.Call("AFTivIPCWScorePre", XX = ZXmat, 
+  #                           delta_vec = delta, 
+  #                           GCT_ = GCT, 
+  #                           icpw_num_denom_ = icpw.num.denom,
+  #                           conf_x_loc_ = conf.x.loc,
+  #      PACKAGE = "aftiv")
+}
+
